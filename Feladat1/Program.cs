58ee2e773442d8874fe2c85d9f8aa80d9ec867f9
notdefinitelynot <ndobany@gmail.com>
2024-03-27 11:16:07 +0100

    Add project files.

---
+++ Feladat1/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Allat
+{
+    public string Name;
+    public string Species;
+    public int BirthYear;
+    public int Weight;
+
+    public Allat()
+    {
+    }
+
+    public Allat(string name, string species, int birthYear, int weight)
+    {
+        Name = name;
+        Species = species;
+        BirthYear = birthYear;
+        Weight = weight;
+    }
+
+    public override string? ToString()
+    {
+        return $"Név: {Name}, Faj: {Species}, SzülÉv: {BirthYear}, Súly: {Weight}kg";
+    }
+
+    static void Main(string[] args)
+    {
+        Allat a = new Allat("Muci","Macska",2023,3);
+        Console.WriteLine(a.ToString());
+    }
+
+
+}
